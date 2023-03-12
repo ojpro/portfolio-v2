b7d7af4c84ba0987f14863a086fec656477a2cff
ojpro <contact@ojpro.me>
2023-03-12 12:56:01 +0100

    added tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import Head from "next/head";
 
-export default function Home(){
+export default function  (){
   return (
     <>
       <Head>
         <title> My New Portfolio - ojpro </title>
       </Head>
+      <main>
+        <h1 className="text-center mt-4">Hey.</h1>
+      </main>
     </>
   );
 }
