8fb3827b5ccf9729bbfe66d4741a6994112cce69
ojpro <contact@ojpro.me>
2023-03-15 12:54:52 +0100

    🌌 simple styling for the about me section.

--- src/components/partials/sections/About.tsx
+++ src/components/partials/sections/About.tsx
@@ -1,26 +1,24 @@
 export default function About() {
   return (
     <>
-      <section>
-        <h2>Who Am I?</h2>
-        <div>
-          <img src="#" alt="My Picture" />
+      <section className="container mx-auto my-6">
+        <h2 className="text-center text-2xl font-bold my-2">Who Am I?</h2>
+        <div className="flex flex-row flex-wrap justify-evenly items-center">
+          <img src="https://source.unsplash.com/random/200x200/?person" className="rounded-full" alt="My Picture" />
           <div>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
-              ducimus eius nostrum aspernatur delectus nam, ratione tempora
+              ducimus eius nostrum<br/> aspernatur delectus nam, ratione tempora
               dolor.
             </p>
-            <div>
-              <a href="#">Contact Me</a>
-              <a href="#">My Resume</a>
+            <div className="flex flex-row flex-nowrap gap-2 justify-start mt-4">
+              <a href="#" className="py-2.5 px-4 bg-blue-500 text-white">Contact Me</a>
+              <a href="#" className="py-2.5 px-4 border border-blue-500 text-blue-500">My Resume</a>
             </div>
           </div>
         </div>
       </section>
-      <div>
-        ----------------------------------------------------------------
-      </div>
+      <div className="bg-gray-100 h-1 w-full my-2"></div>
     </>
   );
 }
