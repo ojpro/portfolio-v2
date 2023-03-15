cfa01b1f0331784d316234d82ed82af7b3f679ac
ojpro <contact@ojpro.me>
2023-03-15 14:58:07 +0100

    simple styling for the work section.

--- src/components/partials/sections/Work.tsx
+++ src/components/partials/sections/Work.tsx
@@ -1,36 +1,39 @@
 export default function Work() {
   return (
     <>
-      <section>
-        <h2>My Work</h2>
+      <section className="container mx-auto">
+        <h2 className="text-2xl text-center">My Work</h2>
         {/* <> Filters </> */}
         <div>
-          <ul>
-            <li>Frontend</li>
-            <li>Backend</li>
-            <li>Full-stack</li>
-            <li>Apps</li>
-            <li>Scripts</li>
+          <ul className="flex flex-row flex-wrap justify-center items-center gap-2">
+            <li><a href="#">All</a></li>
+            <li><a href="#">Websites</a></li>
+            <li><a href="#">Apps</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Scripts</a></li>
           </ul>
         </div>
 
         {/* <> Projects Card </> */}
         <div>
-          <ul>
-            <li>
-              <img src="" alt="" />
+          <ul className="grid-cols-3 p-20 space-y-2 lg:space-y-0 lg:grid lg:gap-3 lg:grid-rows-3">
+            <li className="col-span-2 row-span-2">
+              <img src="https://source.unsplash.com/random" alt="" />
             </li>
             <li>
-              <img src="" alt="" />
+              <img src="https://source.unsplash.com/random" alt="" />
             </li>
             <li>
-              <img src="" alt="" />
+              <img src="https://source.unsplash.com/random" alt="" />
             </li>
             <li>
-              <img src="" alt="" />
+              <img src="https://source.unsplash.com/random" alt="" />
             </li>
             <li>
-              <img src="" alt="" />
+              <img src="https://source.unsplash.com/random/?man" alt="" />
+            </li>
+            <li>
+              <img src="https://source.unsplash.com/random/?man" alt="" />
             </li>
           </ul>
         </div>
