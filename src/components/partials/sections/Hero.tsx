b518bdcc1850ffc62a2cd85df00e6f4330c3700d
ojpro <contact@ojpro.me>
2023-03-15 12:51:58 +0100

    ❄ minor edits on the hero section styling

--- src/components/partials/sections/Hero.tsx
+++ src/components/partials/sections/Hero.tsx
@@ -2,8 +2,8 @@ import {FiChevronDown} from "react-icons/fi"
 
 export default function HeroSection() {
   return (
-    <div className="min-h-[50vh] relative my-2">
-      <section className="mx-auto mt-60 text-center">
+    <div className="min-h-[30vh] relative my-2">
+      <section className="mx-auto mt-40 text-center max-w-3xl">
         <h1 className="text-3xl font-bold leading-2 my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
         <p className="my-4">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto
@@ -14,7 +14,7 @@ export default function HeroSection() {
           <a href="#" className="py-2.5 px-4 bg-blue-500 text-white">let's Start Working</a>
         </div>
       </section>
-      <div className="absolute w-full h-1 bg-gray-200 bottom-0">
+      <div className="w-full h-1 bg-gray-200 my-36 -rotate-1">
           <FiChevronDown className="text-3xl mx-auto cursor-pointer"></FiChevronDown>
       </div>
     </div>
