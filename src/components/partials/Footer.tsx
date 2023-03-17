a2c9710bd36d63d29b6e689a8300215fd15bc47f
ojpro <contact@ojpro.me>
2023-03-17 01:23:07 +0100

    simple footer design

--- src/components/partials/Footer.tsx
+++ src/components/partials/Footer.tsx
@@ -1,37 +1,37 @@
 export default function Footer() {
-  return (
-    <>
-      <footer>
-        {/* <> Links </> */}
-        <div>
-        <ul>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">Project</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
+    return (
+        <>
+            <footer className="bg-gray-900 pt-6 pb-2  mt-6">
+                {/* <> Links </> */}
+                <div className="container mx-auto flex flex-row flex-wrap justify-start gap-4">
+                    <ul>
+                        <li>
+                            <a href="#" className="text-gray-300 hover:text-white">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-gray-300 hover:text-white">Project</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-gray-300 hover:text-white">Contact</a>
+                        </li>
+                    </ul>
 
-          <ul>
-            <li>
-              <a href="#">Sitemap</a>
-            </li>
-            <li>
-              <a href="#">Privacy</a>
-            </li>
-            <li>
-              <a href="#">Report</a>
-            </li>
-          </ul>
-        </div>
+                    <ul>
+                        <li>
+                            <a href="#" className="text-gray-300 hover:text-white">Sitemap</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-gray-300 hover:text-white">Privacy</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-gray-300 hover:text-white">Report</a>
+                        </li>
+                    </ul>
+                </div>
 
-        {/* <> Right </> */}
-        <p> Copy Rights @ 2023 </p>
-      </footer>
-    </>
-  );
+                {/* <> Right </> */}
+                <p className="text-center text-white"> Copy Rights @ 2023 </p>
+            </footer>
+        </>
+    );
 }
