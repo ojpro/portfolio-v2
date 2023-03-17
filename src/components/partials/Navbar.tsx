f745cd630b633348f2e262db752662b4cab4cc38
Oussama ELJabbari <contact@ojpro.me>
2023-03-17 06:34:14 +0000

    make the navbar sticky

--- src/components/partials/Navbar.tsx
+++ src/components/partials/Navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <>
-      <nav className="flex flex-row justify-evenly border leading-8 py-2">
+      <nav className="flex flex-row justify-evenly border leading-8 py-2 sticky top-0 left-0 right-0">
         <a href="#">Logo</a>
         <ul className="flex flex-row justify-evenly gap-3">
           <li className="py-0.5 px-2">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,27 @@
 import BlankLayout from "@/components/layouts/BlankLayout";
-import Header from "@/components/partials/Header";
 import Footer from "@/components/partials/Footer";
 import About from "@/components/partials/sections/About";
 import Work from "@/components/partials/sections/Work";
 import Testimonials from "@/components/partials/sections/Testimonials";
 import Contact from "@/components/partials/sections/Contact";
+import Navbar from "@/components/partials//Navbar";
+import HeroSection from "@/components/partials/sections/Hero";
 
 export default function Home() {
   return (
     <>
       <BlankLayout>
+
+        {/* <> Navbar </> */}
+        <Navbar></Navbar>
+
+
         {/* <> Header </> */}
-        <Header></Header>
+        <header>
+          {/* <> Hero Section </> */}
+          <HeroSection></HeroSection>
+        </header>
+
 
         {/* <> About Section </> */}
         <About></About>
