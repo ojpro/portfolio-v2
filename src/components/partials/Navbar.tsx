9234a490d4eb9688fd98ca320fdd91b55d243801
ojpro <contact@ojpro.me>
2023-03-18 23:37:11 +0100

    styled navbar

--- src/components/partials/Navbar.tsx
+++ src/components/partials/Navbar.tsx
@@ -1,20 +1,20 @@
 export default function Navbar() {
   return (
     <>
-      <nav className="flex flex-row justify-evenly border leading-8 py-2 sticky top-0 left-0 right-0">
-        <a href="#">Logo</a>
+      <nav className="flex flex-row justify-evenly items-center shadow-sm leading-8 py-3 sticky top-0 left-0 right-0 bg-white/90 dark:bg-gray-800/95 dark:text-gray-200 z-50">
+        <a href="#" className="text-lg font-bold">Logo</a>
         <ul className="flex flex-row justify-evenly gap-3">
-          <li className="py-0.5 px-2">
-            <a href="#">About</a>
+          <li>
+            <a href="#" className="inline-block py-0.5 px-2.5 rounded hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:text-white">About</a>
           </li>
-          <li className="py-0.5 px-2">
-            <a href="#">Work</a>
+          <li>
+            <a href="#" className="inline-block py-0.5 px-2.5 rounded hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:text-white">Work</a>
           </li>
-          <li className="py-0.5 px-2">
-            <a href="#">Testimonials</a>
+          <li>
+            <a href="#" className="inline-block py-0.5 px-2.5 rounded hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:text-white">Testimonials</a>
           </li>
-          <li className="py-0.5 px-2">
-            <a href="#">Contact</a>
+          <li>
+            <a href="#" className="inline-block py-0.5 px-2.5 rounded hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:text-white">Contact</a>
           </li>
         </ul>
       </nav>
