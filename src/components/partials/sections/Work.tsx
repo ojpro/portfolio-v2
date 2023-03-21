f63e521979c24491d6e6d291fd772df0a8c57cc0
ojpro <contact@ojpro.me>
2023-03-21 22:54:55 +0000

    re-styling the project cards

--- src/components/partials/sections/Work.tsx
+++ src/components/partials/sections/Work.tsx
@@ -1,44 +1,75 @@
-export default function Work() {
-  return (
-    <>
-      <a href="#" id="work"></a>
-      <section className="container mx-auto py-28">
-        <h2 className="text-2xl text-center my-10">My Work</h2>
-        {/* <> Filters </> */}
-        <div className="my-4">
-          <ul className="flex flex-row flex-wrap justify-center gap-4">
-            <li><a href="#" className="py-1.5 px-3 rounded dark:bg-gray-600">All</a></li>
-            <li><a href="#" className="py-1.5 px-3 rounded dark:bg-gray-800/30">Websites</a></li>
-            <li><a href="#" className="py-1.5 px-3 rounded dark:bg-gray-800/30">Apps</a></li>
-            <li><a href="#" className="py-1.5 px-3 rounded dark:bg-gray-800/30">Games</a></li>
-            <li><a href="#" className="py-1.5 px-3 rounded dark:bg-gray-800/30">Scripts</a></li>
-          </ul>
-        </div>
+import Image from "next/image";
+// import project screenshots
+import executive from "@/../public/images/executive.png";
+import steakhouse from "@/../public/images/steakhouse.png";
+import coliving from "@/../public/images/coliving.png";
 
-        {/* <> Projects Card </> */}
-        <div>
-          <ul className="grid-cols-3 p-4 space-y-2 lg:space-y-0 lg:grid lg:gap-3 lg:grid-rows-3">
-            <li className="col-span-2 row-span-2">
-              <img src="https://source.unsplash.com/random" alt="" />
-            </li>
-            <li>
-              <img src="https://source.unsplash.com/random" alt="" />
-            </li>
-            <li>
-              <img src="https://source.unsplash.com/random" alt="" />
-            </li>
-            <li>
-              <img src="https://source.unsplash.com/random" alt="" />
-            </li>
-            <li>
-              <img src="https://source.unsplash.com/random/?man" alt="" />
-            </li>
-            <li>
-              <img src="https://source.unsplash.com/random/?man" alt="" />
-            </li>
-          </ul>
-        </div>
-      </section>
-    </>
-  );
+export default function Work() {
+    return (
+        <>
+            <a href="#" id="work"></a>
+            <section className="container mx-auto py-28 px-8">
+                <h2 className="text-2xl text-center my-10">My Work</h2>
+                {/* <> Filters </> */}
+                <div className="my-8">
+                    <ul className="flex flex-row flex-wrap justify-center items-center gap-4">
+                        <li><a href="#"
+                               className="py-1.5 px-3 rounded dark:bg-gray-600 border border-transparent hover:border-gray-600 delay-100 ease-linear">All</a>
+                        </li>
+                        <li><a href="#"
+                               className="py-1.5 px-3 rounded dark:bg-gray-800/70 border border-transparent hover:border-gray-600 delay-100 ease-linear">Websites</a>
+                        </li>
+                        <li><a href="#"
+                               className="py-1.5 px-3 rounded dark:bg-gray-800/70 border border-transparent hover:border-gray-600 delay-100 ease-linear">Apps</a>
+                        </li>
+                        <li><a href="#"
+                               className="py-1.5 px-3 rounded dark:bg-gray-800/70 border border-transparent hover:border-gray-600 delay-100 ease-linear">Games</a>
+                        </li>
+                        <li><a href="#"
+                               className="py-1.5 px-3 rounded dark:bg-gray-800/70 border border-transparent hover:border-gray-600 delay-100 ease-linear">Scripts</a>
+                        </li>
+                    </ul>
+                </div>
+
+                {/* <> Projects Card </> */}
+                <div className="w-11/12 mx-auto">
+                    <ul className="flex flex-col justify-between gap-16">
+                        <li className="grid lg:grid-cols-2 p-1 overflow-hidden rounded-xl bg-gradient-to-bl from-[#F3B194] to-[#F30103]">
+                            <div className="self-center container w-10/12 mx-auto">
+                                <h3 className="text-3xl font-bold my-4">Project Name</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aut cum earum et
+                                    eum neque nulla quis saepe tempore.</p>
+                                <a href="#"
+                                   className="my-4 inline-block border px-6 py-2 rounded-full text-black bg-white font-medium">More
+                                    Details </a>
+                            </div>
+                            <Image src={steakhouse} alt="" className="ml-28"/>
+                        </li>
+                        <li className="grid lg:grid-cols-2 p-1 overflow-hidden rounded-xl bg-gradient-to-bl from-[#A2A0FE] to-[#27187E]">
+                            <div className="self-center container w-10/12 mx-auto order-2">
+                                <h3 className="text-3xl font-bold my-4">Project Name</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aut cum earum et
+                                    eum neque nulla quis saepe tempore.</p>
+                                <a href="#"
+                                   className="my-4 inline-block border px-6 py-2 rounded-full text-black bg-white font-medium">More
+                                    Details </a>
+                            </div>
+                            <Image src={executive} alt="" className="-ml-28"/>
+                        </li>
+                        <li className="grid lg:grid-cols-2 p-1 overflow-hidden rounded-xl bg-gradient-to-bl from-[#EA4E2E] to-[#EA720C]">
+                            <div className="self-center container w-10/12 mx-auto">
+                                <h3 className="text-3xl font-bold my-4">Project Name</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aut cum earum et
+                                    eum neque nulla quis saepe tempore.</p>
+                                <a href="#"
+                                   className="my-4 inline-block border px-6 py-2 rounded-full text-black bg-white font-medium">More
+                                    Details </a>
+                            </div>
+                            <Image src={coliving} alt="" className="p-4"/>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </>
+    );
 }
