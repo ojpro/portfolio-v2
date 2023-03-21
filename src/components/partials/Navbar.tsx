c1acd50c36a52194081b18958015c4247c9f968b
ojpro <contact@ojpro.me>
2023-03-21 00:21:35 +0000

    clean up

--- src/components/partials/Navbar.tsx
+++ src/components/partials/Navbar.tsx
@@ -1,20 +1,20 @@
 export default function Navbar() {
   return (
     <>
-      <nav className="flex flex-row justify-evenly items-center shadow-sm leading-8 py-3 sticky top-0 left-0 right-0 border-y border-gray-800 dark:bg-gray-900/90 z-50">
+      <nav className="flex flex-row justify-evenly items-center shadow-sm leading-8 py-3 sticky top-0 left-0 right-0 border-y border-gray-800 dark:bg-gray-900 z-50">
         <a href="#" className="text-lg font-bold">Logo</a>
         <ul className="flex flex-row justify-evenly gap-3 group">
           <li>
-            <a href="#about" className="inline-block py-0.5 px-2.5 group-hover:text-gray-400 rounded hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:text-white delay-100 ease-in-out">About</a>
+            <a href="#about" className="inline-block py-0.5 px-2.5 group-hover:text-gray-400 dark:hover:text-white delay-100 ease-in-out">About</a>
           </li>
           <li>
-            <a href="#work" className="inline-block py-0.5 px-2.5 group-hover:text-gray-400 rounded hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:text-white delay-100 ease-in-out">Work</a>
+            <a href="#work" className="inline-block py-0.5 px-2.5 group-hover:text-gray-400 dark:hover:text-white delay-100 ease-in-out">Work</a>
           </li>
           <li>
-            <a href="#testimonials" className="inline-block py-0.5 group-hover:text-gray-400 px-2.5 rounded hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:text-white delay-100 ease-in-out">Testimonials</a>
+            <a href="#testimonials" className="inline-block py-0.5 group-hover:text-gray-400 px-2.5 dark:hover:text-white delay-100 ease-in-out">Testimonials</a>
           </li>
           <li>
-            <a href="#contact" className="inline-block py-0.5 px-2.5 group-hover:text-gray-400 rounded hover:bg-gray-100 dark:hover:bg-gray-900 dark:hover:text-white delay-100 ease-in-out">Contact</a>
+            <a href="#contact" className="inline-block py-0.5 px-2.5 group-hover:text-gray-400 dark:hover:text-white delay-100 ease-in-out">Contact</a>
           </li>
         </ul>
       </nav>
