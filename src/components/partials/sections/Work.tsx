250c869794392fa42b21d56355327b84a05567ed
ojpro <contact@ojpro.me>
2023-03-22 00:34:28 +0000

    refactored the projects' card

--- src/components/partials/sections/Work.tsx
+++ src/components/partials/sections/Work.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image";
+// Import components
+import ProjectCard from "@/components/partials/cards/ProjectCard";
+
 // import project screenshots
 import executive from "@/../public/images/executive.png";
 import steakhouse from "@/../public/images/steakhouse.png";
@@ -8,48 +11,26 @@ export default function Work() {
     return (
         <>
             <a href="#" id="work"></a>
-            <section className="container mx-auto py-28 px-8">
-                <h2 className="text-2xl text-center my-10">My Work</h2>
+            <section className="container mx-auto px-8">
+                <h2 className="text-5xl font-bold text-center mb-20 mt-10">My Work</h2>
 
                 {/* <> Projects Card </> */}
                 <div className="w-11/12 mx-auto">
-                    <ul className="flex flex-col justify-between gap-16">
-                        <li className="grid lg:grid-cols-2 p-1 overflow-hidden rounded-xl bg-gradient-to-bl from-[#F3B194] to-[#F30103]">
-                            <div className="self-center container w-10/12 mx-auto">
-                                <h3 className="text-3xl font-bold my-4">Project Name</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aut cum earum et
-                                    eum neque nulla quis saepe tempore.</p>
-                                <a href="#"
-                                   className="my-4 inline-block border px-6 py-2 rounded-full text-black bg-white font-medium">More
-                                    Details </a>
-                            </div>
-                            <Image src={steakhouse} alt="" className="ml-28"/>
-                        </li>
-                        <li className="grid lg:grid-cols-2 p-1 overflow-hidden rounded-xl bg-gradient-to-bl from-[#A2A0FE] to-[#27187E]">
-                            <div className="self-center container w-10/12 mx-auto order-2">
-                                <h3 className="text-3xl font-bold my-4">Project Name</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aut cum earum et
-                                    eum neque nulla quis saepe tempore.</p>
-                                <a href="#"
-                                   className="my-4 inline-block border px-6 py-2 rounded-full text-black bg-white font-medium">More
-                                    Details </a>
-                            </div>
-                            <Image src={executive} alt="" className="-ml-28"/>
-                        </li>
-                        <li className="grid lg:grid-cols-2 p-1 overflow-hidden rounded-xl bg-gradient-to-bl from-[#EA4E2E] to-[#EA720C]">
-                            <div className="self-center container w-10/12 mx-auto">
-                                <h3 className="text-3xl font-bold my-4">Project Name</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aut cum earum et
-                                    eum neque nulla quis saepe tempore.</p>
-                                <a href="#"
-                                   className="my-4 inline-block border px-6 py-2 rounded-full text-black bg-white font-medium">More
-                                    Details </a>
-                            </div>
-                            <Image src={coliving} alt="" className="p-4"/>
-                        </li>
+                    <ul className="flex flex-col justify-between gap-24">
+                        <ProjectCard title="Project Title" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aut cum earum et
+                                    eum neque nulla quis saepe tempore." image={steakhouse} bgFrom="#F3B194"
+                                     bgTo="#F30103"></ProjectCard>
+                        <ProjectCard title="Project Title" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aut cum earum et
+                                    eum neque nulla quis saepe tempore." image={executive} bgFrom="#A2A0FE"
+                                     bgTo="#27187E"></ProjectCard>
+                        <ProjectCard title="Project Title" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aut cum earum et
+                                    eum neque nulla quis saepe tempore." image={coliving} bgFrom="#EA4E2E"
+                                     bgTo="#EA720C"
+                        ></ProjectCard>
                     </ul>
                 </div>
             </section>
         </>
-    );
+    )
+        ;
 }
