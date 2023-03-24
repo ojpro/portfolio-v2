d47adb40d8c7a53e479735f589c615dbaa2c5629
ojpro <contact@ojpro.me>
2023-03-24 03:53:33 +0000

    ✨ styled the about section component

--- src/components/partials/sections/About.tsx
+++ src/components/partials/sections/About.tsx
@@ -1,4 +1,6 @@
 import Navbar from "@/components/partials/Navbar";
+import Image from "next/image";
+import MyPicture from "@/../public/images/my-picture.png";
 
 export default function About() {
     return (
@@ -6,23 +8,30 @@ export default function About() {
             {/* <> Navbar </> */}
             <Navbar></Navbar>
             <a href="#" id="about"></a>
-            <div className="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2">
-                <h2 className="text-center text-2xl font-bold my-2">Who Am I?</h2>
-                <div className="flex flex-row flex-wrap justify-evenly items-center">
-                    <img src="https://source.unsplash.com/random/200x200/?person" className="rounded-full"
-                         alt="My Picture"/>
-                    <div>
-                        <p>
-                            Hey, I&apos;m Oussama, Full-Stack Developer. I work on building my clients&apos; ideas as a
+            <div className="m-20">
+                <h2 className="text-5xl font-bold text-center mb-20 mt-10">Who Am I?</h2>
+                <div
+                    className="container mx-auto p-4 flex flex-row flex-wrap justify-evenly items-center min-h-full">
+                    <div className="max-w-2xl flex flex-col justify-between gap-4">
+                        <h3 className="text-3xl font-medium">Hey,</h3>
+                        <p className="">
+                            I&apos;m Oussama, Full-Stack Developer. I work on building my clients&apos; ideas
+                            as a
                             freelancer.
                             As a Developer, I am very passionate when it comes to solving problems, Planning, and
                             designing ideas.
                             Besides my job, I play chess, draw things, and listen to Spotify.
                         </p>
-                        <div className="flex flex-row flex-nowrap gap-2 justify-start mt-4">
-                            <a href="#contact" className="py-2.5 px-4 bg-blue-500 text-white">Contact Me</a>
-                            <a href="#" className="py-2.5 px-4 border border-blue-500 text-blue-500">My Resume</a>
+                        <div className="flex flex-row flex-nowrap gap-4 justify-start mt-4">
+                            <a href="#contact" className="py-2.5 px-4 bg-[#4462f5] text-white rounded-full">Let&apos;s work</a>
+                            <a href="#" className="py-2.5 px-4 border border-[#4462f5] text-[#4462f5] rounded-full">Want My
+                                Resume ?</a>
                         </div>
+                    </div>
+                    <div className="relative bg-gray-800 backdrop-blur-md dark:bg-opacity-60 hover:dark:bg-opacity-80 rounded-xl p-1 hover:rotate-2 shadow-2xl m-10">
+                        <Image src={MyPicture}
+                               className="w-80 h-fit rounded-2xl rotate-3 hover:-rotate-6 shadow-2xl backdrop-blur-md md:opacity-75 hover:opacity-100 hover:scale-105"
+                               alt="My Picture"/>
                     </div>
                 </div>
             </div>
