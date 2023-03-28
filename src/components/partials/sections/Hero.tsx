622b292c15a62d1e6a29d65280a8444a5956ddc5
ojpro <contact@ojpro.me>
2023-03-28 00:56:43 +0000

    edits on the hero section style

--- src/components/partials/sections/Hero.tsx
+++ src/components/partials/sections/Hero.tsx
@@ -1,19 +1,29 @@
-import {FiChevronDown} from "react-icons/fi"
+import Image from "next/image";
+
+// import hero illustration image
+import IllustrationImage from "@/../public/images/hero-section-illustration.svg";
+import Navbar from "@/components/partials/Navbar";
 
 export default function HeroSection() {
-  return (
-    <section className="min-h-screen relative">
-      <div className="mx-auto text-center max-w-3xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-2/3">
-        <h1 className="text-3xl font-bold leading-2 my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
-        <p className="my-4">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto
-          sapiente recusandae eaque, quis ab velit nostrum animi fuga sit atque
-          voluptatem, in perferendis inventore cumque?
-        </p>
-        <div className="mt-6">
-          <a href="#contact" className="py-2.5 px-4 bg-blue-500 text-white">let's Start Working</a>
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <>
+            <section className="h-[80vh] relative flex items-center bg-black/20">
+                <div className="flex flex-row flex-wrap justify-evenly items-center gap-8 w-full p-8 pt-0">
+                    <div className="max-w-lg flex flex-col gap-2">
+                        <h1 className="text-lg">Hey, I am Oussama 👨‍💻.</h1>
+                        <h2 className="text-3xl font-medium">A Full-Stack Developer.</h2>
+                        <h3 className="text-xl text-gray-200">I will help you bring your ideas to a professional
+                            website.</h3>
+                    </div>
+                    <Image src={IllustrationImage} alt="Website Development Services"
+                           className="w-full md:w-1/3 h-auto"/>
+                </div>
+                <svg preserveAspectRatio="none" width="1440" height="74" viewBox="0 0 1440 74"
+                     className="absolute -bottom-[74px] left-0 right-0 rotate-180 w-full fill-black/20">
+                    <path
+                        d="M456.464 0.0433865C277.158 -1.70575 0 50.0141 0 50.0141V74H1440V50.0141C1440 50.0141 1320.4 31.1925 1243.09 27.0276C1099.33 19.2816 1019.08 53.1981 875.138 50.0141C710.527 46.3727 621.108 1.64949 456.464 0.0433865Z"></path>
+                </svg>
+            </section>
+        </>
+    );
 }
