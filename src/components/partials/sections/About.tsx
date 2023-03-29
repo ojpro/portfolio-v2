8a01d0e989fb7f706d880287311e3d2be9cb02fb
Oussama ELJabbari <contact@ojpro.me>
2023-03-29 09:24:49 +0000

    Responsive issues fixes, Closed #10

--- src/components/partials/sections/About.tsx
+++ src/components/partials/sections/About.tsx
@@ -11,8 +11,8 @@ export default function About() {
             <Navbar></Navbar>
             <a href="#" id="about"></a>
             <section className="min-h-screen relative">
-                <div className="absolute inset-0 m-auto p-20">
-                    <h2 className="text-5xl font-bold text-center">Who Am I?</h2>
+                <div className="m-4">
+                    <h2 className="text-5xl font-bold text-center pt-20 mb-12 lg:mb-8">Who Am I?</h2>
                     <div
                         className="h-5/6 mx-auto p-4 flex flex-row flex-wrap justify-evenly items-center self-center">
                         <div className="max-w-3xl flex flex-col justify-between gap-4">
@@ -26,7 +26,7 @@ export default function About() {
                                 I&apos;m Oussama, a Full-Stack Developer. I work as a freelancer, using my skills to build my clients&apos; ideas. As a Developer, I am passionate about problem-solving, planning, and designing. In my spare time, I enjoy playing chess, drawing, and listening to Spotify.
                                 </p>
                             </div>
-                            <div className="flex flex-row flex-nowrap gap-4 justify-start mt-4">
+                            <div className="flex flex-row flex-wrap gap-4 justify-start mt-4">
                                 <a href="#contact"
                                     className="py-2.5 px-4 bg-[#4462f5] text-white rounded-md flex flex-row flex-nowrap justify-evenly items-center gap-2 hover:py-3 hover:px-5 group">
                                     Let&apos;s work
@@ -34,15 +34,15 @@ export default function About() {
                                         className="w-6 h-auto group-hover:animate-ping"></BsArrowRightShort>
                                 </a>
                                 <a href="#"
-                                    className="py-2.5 px-4 bg-gray-800 text-gray-300 rounded-md flex flex-row flex-nowrap justify-evenly items-center gap-2 border-2 border-transparent hover:border-2 hover:border-blue-600 hover:translate-x-0.5 group">
+                                    className="py-2.5 px-4 bg-gray-800 text-gray-300 rounded-md flex flex-row flex-nowrap justify-evenly items-center gap-2 border-2 border-transparent hover:border-blue-600 hover:translate-x-0.5 group">
                                     Want My Resume ?
                                     <FiExternalLink
-                                        className="group-hover:text-blue-400 hidden group-hover:inline-block"></FiExternalLink>
+                                        className="group-hover:text-blue-400 invisible group-hover:visible"></FiExternalLink>
                                 </a>
                             </div>
                         </div>
                         <div
-                            className="relative bg-gray-800 backdrop-blur-md dark:bg-opacity-80 hover:dark:bg-opacity-80 rounded-xl p-1 hover:rotate-2 shadow-2xl m-16">
+                            className="relative bg-gray-800 backdrop-blur-md dark:bg-opacity-80 hover:dark:bg-opacity-80 rounded-xl p-1 hover:rotate-2 shadow-2xl m-16 min-w-[300px]">
                             <Image src={MyPicture}
                                 className="w-80 h-fit rounded-2xl rotate-3 hover:-rotate-6 shadow-2xl backdrop-blur-md md:opacity-80 hover:opacity-100 hover:scale-105 contrast-125"
                                 alt="My Picture" />
