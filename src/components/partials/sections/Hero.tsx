1d7a7ea417a52a9d706b8efc36c53b7a4db1be7d
Oussama ELJabbari <contact@ojpro.me>
2023-03-29 13:37:54 +0000

    updated the UI of the hero and about section

--- src/components/partials/sections/Hero.tsx
+++ src/components/partials/sections/Hero.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
+import { FiChevronDown } from "react-icons/fi";
 
 // import hero illustration image
 import IllustrationImage from "@/../public/images/hero-section-illustration.svg";
-import Navbar from "@/components/partials/Navbar";
 
 export default function HeroSection() {
     return (
         <>
-            <section className="h-[80vh] relative flex items-center bg-black/20">
+            <section className="h-[80vh] relative flex items-center bg-black/40">
                 <div className="flex flex-row flex-wrap justify-evenly items-center gap-8 w-full p-8 pt-0">
                     <div className="max-w-lg flex flex-col gap-2">
                         <h1 className="text-lg">Hey, I am Oussama 👨‍💻.</h1>
@@ -16,14 +16,18 @@ export default function HeroSection() {
                             website.</h3>
                     </div>
                     <Image src={IllustrationImage} alt="Website Development Services"
-                           className="w-full md:w-1/3 h-auto"/>
+                        className="w-full md:w-1/3 h-auto" />
                 </div>
                 <svg preserveAspectRatio="none" width="1440" height="74" viewBox="0 0 1440 74"
-                     className="absolute -bottom-[74px] left-0 right-0 rotate-180 w-full fill-black/20">
+                    className="absolute -bottom-[74px] left-0 right-0 rotate-180 w-full fill-black/40">
                     <path
                         d="M456.464 0.0433865C277.158 -1.70575 0 50.0141 0 50.0141V74H1440V50.0141C1440 50.0141 1320.4 31.1925 1243.09 27.0276C1099.33 19.2816 1019.08 53.1981 875.138 50.0141C710.527 46.3727 621.108 1.64949 456.464 0.0433865Z"></path>
                 </svg>
             </section>
+            <a href="#about"
+                className="bg-gray-800 block w-fit mx-auto p-2 rounded-full shadow-2xl hover:bg-gray-800/50 hover:shadow-none hover:translate-y-0.5 hover:delay-200 hover:ease-linear animate-pulse hover:animate-none border-2 border-gray-700/40">
+                <FiChevronDown className="w-8 h-8 text-gray-200"></FiChevronDown>
+            </a>
         </>
     );
 }
