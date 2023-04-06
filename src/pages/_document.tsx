e6bab00d830abc9cf495a7aeff078268a8a82a56
ojpro <contact@ojpro.me>
2023-04-06 16:54:26 +0000

    added default title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import {Html, Head, Main, NextScript} from 'next/document'
+import {Head, Html, Main, NextScript} from 'next/document'
 
 export default function Document() {
     return (
@@ -6,6 +6,7 @@ export default function Document() {
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <title>My Portfolio 💻 - ojpro </title>
                 <link
                     href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600&display=swap"
                     rel="stylesheet"/>
