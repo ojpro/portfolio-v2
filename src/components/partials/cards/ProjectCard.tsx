8eae316ead573a346ffed363a4aca1246cbf3a52
ojpro <contact@ojpro.me>
2023-04-06 16:45:35 +0000

    added alt attribute to work cards' image

--- src/components/partials/cards/ProjectCard.tsx
+++ src/components/partials/cards/ProjectCard.tsx
@@ -3,7 +3,7 @@ import {RxExternalLink} from "react-icons/rx";
 import {HiOutlineCodeBracket} from "react-icons/hi2";
 import {BiChevronRight} from "react-icons/bi";
 
-export default function ProjectCard(props: { title: string, description: string, image: StaticImageData, bgFrom: string, bgTo: string }) {
+export default function ProjectCard(props: { title: string, description: string, image: StaticImageData, imageAlt: string, bgFrom: string, bgTo: string }) {
     return (
         <li className={"grid lg:grid-cols-2 p-1 group overflow-hidden rounded-xl bg-gradient-to-bl " + props.bgFrom + " " + props.bgTo}>
             <div className="self-center container w-10/12 mx-auto lg:group-even:order-2">
@@ -16,7 +16,7 @@ export default function ProjectCard(props: { title: string, description: string,
                 </a>
             </div>
             <div className="relative group">
-                <Image src={props.image} alt="" className=""/>
+                <Image src={props.image} alt={props.imageAlt} className=""/>
                 <ul className="lg:absolute lg:top-1/2 lg:left-1/2 lg:-translate-y-1/2 lg:-translate-x-1/2 lg:invisible lg:p-16 p-8 rounded min-w-max group-hover:visible delay-300 ease-in-out flex md:flex-row flex-wrap gap-4 justify-between items-start">
                     <li className="bg-gradient-to-bl from-gray-600 to-gray-800/50 hover:to-gray-900 border border-transparent hover:border-gray-700 hover:delay-300 ease-linear rounded-full mr-4">
                         <a href="#"
