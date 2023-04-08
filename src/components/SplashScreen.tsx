b3bbf8a579cd3ff88c68d83979ba3ebf610268c3
ojpro <contact@ojpro.me>
2023-04-08 00:58:22 +0000

    updated the loading screen, Closes #40

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -2,8 +2,8 @@ export default function SplashScreen() {
 
     return (
         <div
-            className="bg-gray-800 flex min-h-screen w-full justify-center items-center text-white text-4xl font-bold">
-            <div className="w-20 h-20 border-[6px] rounded-full border-gray-900 border-t-blue-600 animate-spin"></div>
+            className="bg-gray-900 flex min-h-screen w-full justify-center items-center text-white text-4xl font-bold">
+            <div className="w-20 h-20 border-[6px] border-transparent border-t-blue-600 border-l-2 border-l-blue-600 rounded-full animate-spin"></div>
         </div>
     )
         ;
