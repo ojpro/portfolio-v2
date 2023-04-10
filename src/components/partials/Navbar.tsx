baf7048d692e0a79c7593d3ca7db4108a97ecbec
Oussama ELJabbari <contact@ojpro.me>
2023-04-10 09:39:28 +0000

    Tiny fix, Closes #60

--- src/components/partials/Navbar.tsx
+++ src/components/partials/Navbar.tsx
@@ -91,7 +91,7 @@ export default function Example() {
                                     href={item.href}
                                     aria-label={item.name}
                                     title={item.name}
-                                    className={`text-gray-300 ${item.current ? 'bg-gray-800' : ''} hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium`}
+                                    className={`text-gray-300 ${item.current ? 'bg-gray-800' : ''} hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-md xl:text-sm font-medium`}
                                     aria-current={item.current ? 'page' : undefined}
                                 >
                                     {item.name}
