532bb2f3f9f55c63511db96580b3342dd12de94b
Oussama ELJabbari <contact@ojpro.me>
2023-04-10 12:21:21 +0000

    contact form validation, Closes #69

--- src/components/partials/sections/Contact.tsx
+++ src/components/partials/sections/Contact.tsx
@@ -1,6 +1,6 @@
 import { FiGithub, FiInstagram, FiLinkedin, FiSend, FiTwitter } from "react-icons/fi";
 import { HiOutlineMail } from "react-icons/hi";
-import { useState } from "react";
+import { ChangeEventHandler, useState } from "react";
 import Link from "next/link";
 
 // Import SweetAlert
@@ -11,7 +11,97 @@ export default function Contact() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
-    const [isSendingEmail, setIsSendingEmail] = useState(false);
+    const [canSubmit, setCanSubmit] = useState(true);
+
+    // validation states
+    const [validName, setValidName] = useState<any>(true);
+    const [validEmail, setValidEmail] = useState<any>(true);
+    const [validMessage, setValidMessage] = useState<any>(true);
+
+    // client name validation
+    const validateName = (e: any) => {
+        // store the given name
+        let name = e.target.value;
+
+        // update the state
+        setName(name);
+
+        // check if empty
+        if (name == '') {
+            // display error message
+            setValidName("Your name is important");
+
+            // disable form submit
+            setCanSubmit(false);
+        }
+        // check if less than 3 chars
+        else if (name.length < 3) {
+            // show error message
+            setValidName("The provided name is too short");
+
+            // disable form submit
+            setCanSubmit(false);
+        }
+        else {
+            // otherwise back to default
+            setValidName(true);
+            
+            // enable form
+            setCanSubmit(true);
+        }
+    }
+
+    // client email validation
+    const validateEmail = (e: any) => {
+        // valid email RegExp
+        const regexp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+
+        // store the given email
+        let email = e.target.value;
+
+        // update the state
+        setEmail(email);
+
+        if (!regexp.test(email)) {
+            // error message
+            setValidEmail("The provided email is not valid yet!");
+
+            // disable form submit
+            setCanSubmit(false);
+        }
+        else {
+            // otherwise back to defaul
+            setValidEmail(true);
+
+            // enable form
+            setCanSubmit(true);
+        }
+    }
+
+    // client message validation
+    const validateMessage = (e: any) => {
+
+        // store the given email
+        let message = e.target.value;
+
+        // update the state
+        setMessage(message);
+
+        if (message.length < 25) {
+            // show error message
+            setValidMessage("Can you give me more details ?");
+
+            // disable form submit
+            setCanSubmit(false);
+        }
+        else {
+            // otherwise back to defaul
+            setValidMessage(true);
+
+            // enable form
+            setCanSubmit(true);
+        }
+    }
 
     // form submitting function
     const handleSubmit = async (e: any) => {
@@ -19,9 +109,7 @@ export default function Contact() {
         e.preventDefault();
 
         // disable submit button with a little effects
-        setIsSendingEmail(true);
-
-        // TODO: validate the form
+        setCanSubmit(false);
 
         // send email
         const res = await fetch("/api/sendgrid", {
@@ -43,8 +131,8 @@ export default function Contact() {
             )
 
             // after the modal dimiss return the button to it state
-            setIsSendingEmail(false);
-            
+            setCanSubmit(true);
+
             // clear all the inputs
             setName('');
             setEmail('');
@@ -112,27 +200,30 @@ export default function Contact() {
                         className="flex flex-col flex-nowrap justify-start items-center gap-6">
                         <label htmlFor="name" className="flex flex-col gap-2 w-full">
                             <span>Full Name </span>
-                            <input type="text" value={name} onChange={(e) => setName(e.target.value)}
-                                className="px-3 py-2.5 rounded-md bg-gray-800 outline-none border-2 border-gray-700 focus:border-blue-600"
+                            <input type="text" value={name} onChange={validateName}
+                                className={`px-3 py-2.5 rounded-md bg-gray-800 outline-none border-2 focus:border-blue-600  ${validName == true ? 'border-gray-700' : 'border-red-400'}`}
                                 id="name" required />
+                            <span className="text-sm text-red-400 font-medium">{validName}</span>
                         </label>
                         <label htmlFor="email" className="flex flex-col gap-2 w-full">
                             <span>Email </span>
-                            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}
-                                className="px-3 py-2.5 rounded-md bg-gray-800 outline-none border-2 border-gray-700 focus:border-blue-600"
+                            <input type="email" value={email} onChange={validateEmail}
+                                className={`px-3 py-2.5 rounded-md bg-gray-800 outline-none border-2 focus:border-blue-600  ${validEmail == true ? 'border-gray-700' : 'border-red-400'}`}
                                 id="email" required />
+                            <span className="text-sm text-red-400 font-medium">{validEmail}</span>
                         </label>
                         <label htmlFor="message" className="flex flex-col gap-2 w-full">
                             <span>Message </span>
-                            <textarea name="" id="message" value={message} onChange={(e) => setMessage(e.target.value)}
-                                className="px-3 py-2.5 rounded-md bg-gray-800 outline-none border-2 border-gray-700 focus:border-blue-600 h-24"
+                            <textarea name="" id="message" value={message} onChange={validateMessage}
+                                className={`px-3 py-2.5 rounded-md bg-gray-800 outline-none border-2 ${validMessage == true ? 'border-gray-700' : 'border-red-400'} focus:border-blue-600 h-24`}
                                 required></textarea>
+                            <span className="text-sm text-red-400 font-medium">{validMessage}</span>
                         </label>
 
                         <button
-                            className={`px-7 py-2 rounded self-start group hover:shadow hover:shadow-blue-600 ${isSendingEmail ? 'bg-blue-400 cursor-not-allowed' : 'bg-blue-600'}`}>
+                            className={`px-7 py-2 rounded self-start group hover:shadow hover:shadow-blue-600 ${canSubmit ? 'bg-blue-600' : 'bg-blue-400 cursor-not-allowed'}`}>
                             Send
-                            <FiSend className={`inline-block ml-2 group-hover:fill-white ${isSendingEmail ? 'animate-pulse' : ''}`}></FiSend></button>
+                            <FiSend className={`inline-block ml-2 group-hover:fill-white ${!canSubmit ? 'animate-pulse' : ''}`}></FiSend></button>
                     </form>
                 </div>
             </div>
