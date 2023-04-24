3b7900570fffa87030569bf4d760bfcaaf08f527
Oussama ELJabbari <contact@ojpro.me>
2023-04-24 07:10:15 +0000

    added google analytics support, Closes #74.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
 import '@/styles/globals.css'
-import type {AppProps} from 'next/app';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import Script from 'next/script';
 
-export default function App({Component, pageProps}: AppProps) {
-    return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <Head>
+                <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+                <Script strategy="lazyOnload">
+                    {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+                });
+            `}
+                </Script>
+            </Head>
+
+            <Component {...pageProps} />
+        </>
+    )
 }
