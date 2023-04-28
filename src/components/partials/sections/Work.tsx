64285299e1cce9e310096409a16315fb89ff5669
ojpro <contact@ojpro.me>
2023-04-28 13:03:35 +0100

    added projects link, Closes #98

--- src/components/partials/sections/Work.tsx
+++ src/components/partials/sections/Work.tsx
@@ -13,22 +13,27 @@ export default function Work() {
             <section className="container mx-auto px-8">
                 <h2 className="h2">My Work</h2>
 
+                {/* TODO: make the list of the project dynamic */}
                 {/* <> Projects Card </> */}
                 <div className="w-11/12 mx-auto">
                     <ul className="flex flex-col justify-between gap-16">
                         <ProjectCard title="SteakHouse" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aut cum earum et
                                     eum neque nulla quis saepe tempore." image={steakhouse}
                                      imageAlt="SteakHouse Project Screenshot" bgFrom="from-[#F3B194]"
-                                     bgTo="to-[#F30103]"></ProjectCard>
+                                     bgTo="to-[#F30103]" previewLink="https://ojpro-steakhouse.vercel.app/"
+                                     githubLink="https://github.com/oussamaeljabbari/SteakHouse"/>
+
                         <ProjectCard title="Executive" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aut cum earum et
                                     eum neque nulla quis saepe tempore." image={executive}
                                      imageAlt="Executive Project Screenshot" bgFrom="from-[#A2A0FE]"
-                                     bgTo="to-[#27187E]"></ProjectCard>
+                                     bgTo="to-[#27187E]" previewLink="https://ojpro-executive.vercel.app/"
+                                     githubLink="https://github.com/oussamaeljabbari/executive"/>
+
                         <ProjectCard title="Coliving" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aut cum earum et
                                     eum neque nulla quis saepe tempore." image={coliving}
                                      imageAlt="Coliving Project Screenshot" bgFrom="from-[#EA4E2E]"
-                                     bgTo="to-[#EA720C]"
-                        ></ProjectCard>
+                                     bgTo="to-[#EA720C]" previewLink="https://ojpro-coliving.vercel.app/"
+                                     githubLink="https://github.com/oussamaeljabbari/coliving"/>
                     </ul>
                 </div>
             </section>
