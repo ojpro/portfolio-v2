1ded001a0f2e2941de7161f979fd814aac627b4d
ojpro <contact@ojpro.me>
2023-04-28 13:05:13 +0100

    edit the project description (in progress)

--- src/components/partials/sections/Work.tsx
+++ src/components/partials/sections/Work.tsx
@@ -17,20 +17,17 @@ export default function Work() {
                 {/* <> Projects Card </> */}
                 <div className="w-11/12 mx-auto">
                     <ul className="flex flex-col justify-between gap-16">
-                        <ProjectCard title="SteakHouse" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aut cum earum et
-                                    eum neque nulla quis saepe tempore." image={steakhouse}
+                        <ProjectCard title="SteakHouse" description="SteakHouse - Discovery and get the best steak." image={steakhouse}
                                      imageAlt="SteakHouse Project Screenshot" bgFrom="from-[#F3B194]"
                                      bgTo="to-[#F30103]" previewLink="https://ojpro-steakhouse.vercel.app/"
                                      githubLink="https://github.com/oussamaeljabbari/SteakHouse"/>
 
-                        <ProjectCard title="Executive" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aut cum earum et
-                                    eum neque nulla quis saepe tempore." image={executive}
+                        <ProjectCard title="Executive" description="Executive - We Help to Build Your Dream" image={executive}
                                      imageAlt="Executive Project Screenshot" bgFrom="from-[#A2A0FE]"
                                      bgTo="to-[#27187E]" previewLink="https://ojpro-executive.vercel.app/"
                                      githubLink="https://github.com/oussamaeljabbari/executive"/>
 
-                        <ProjectCard title="Coliving" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam aut cum earum et
-                                    eum neque nulla quis saepe tempore." image={coliving}
+                        <ProjectCard title="Coliving" description="Coliving - Book affordable and hassle-free coliving spaces, all amenities included." image={coliving}
                                      imageAlt="Coliving Project Screenshot" bgFrom="from-[#EA4E2E]"
                                      bgTo="to-[#EA720C]" previewLink="https://ojpro-coliving.vercel.app/"
                                      githubLink="https://github.com/oussamaeljabbari/coliving"/>
